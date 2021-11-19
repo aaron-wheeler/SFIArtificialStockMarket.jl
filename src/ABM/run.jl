66f2b9db8f1c90b24b42b6b858c8ad679a36c95c
aaron-wheeler <77795743+aaron-wheeler@users.noreply.github.com>
2021-11-18 19:58:13 -0500

    test to mess with model data collection

--- src/ABM/run.jl
+++ src/ABM/run.jl
@@ -71,10 +71,14 @@ function let_it_run()
     models = [init_model(; seed, properties...) for seed in seeds] # run entire model for each random seed?
 
     # # Collect data (ensemble simulation for multiple random seeded models)
-    model_runs = 260000 # total numder of time steps in model
-    steady_state = collect(250000:260000) # time steps where data is collected and stored locally
+    # model_runs = 260000 # total numder of time steps in model
+    # steady_state = collect(250000:260000) # time steps where data is collected and stored locally
+    # adf, mdf = ensemblerun!(models, dummystep, model_step!, model_runs;
+    #     adata = adata, mdata = mdata, when = steady_state, when_model = steady_state, parallel = false)
+    
+    model_runs = 100 # total numder of time steps in model
     adf, mdf = ensemblerun!(models, dummystep, model_step!, model_runs;
-        adata = adata, mdata = mdata, when = steady_state, when_model = steady_state, parallel = false)
+        adata = adata, mdata = mdata, parallel = false)
 
     # Collect data (for single model case)
     # adf, mdf = run!(models, dummystep, model_step!, 500;
