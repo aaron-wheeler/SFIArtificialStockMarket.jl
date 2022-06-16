5a2e894011007ca8cd20a031a5a24d0967e08d50
Documenter.jl <documenter@juliadocs.github.io>
2022-06-16 23:20:41 +0000

    build based on a0d6f44

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SFIArtificialStockMarket.jl","page":"SFIArtificialStockMarket.jl","title":"SFIArtificialStockMarket.jl","text":"","category":"section"},{"location":"","page":"SFIArtificialStockMarket.jl","title":"SFIArtificialStockMarket.jl","text":"Documentation for SFIArtificialStockMarket.jl","category":"page"}]
+}
